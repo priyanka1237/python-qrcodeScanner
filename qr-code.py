9bf495248d437ad98e24cf5fab4f8e3cdc5c40cf
priyanka sharma <priyanka95sharma24@gmail.com>
2019-08-26 12:54:29 +0530

    initial commit

---
+++ qr-code.py
@@ -0,0 +1,12 @@
+import qrcode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data('Some data')
+qr.make(fit=True)
+img = qr.make_image()
+img.save('/home/netset/Desktop/index.jpeg')
+img.show()
